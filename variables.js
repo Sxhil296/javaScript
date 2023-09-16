166dbfb16109583d02fe4dc9be56f7cea563593c
Sahil <leviack629@gmail.com>
2023-09-16 18:15:27 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accId = 23763
+let accName = "Emir"
+var accPassword = "123456"
+accCity = "Saharanpur"
+let accState;
+
+// accId = 23   not allowed
+// console.log(accId)
+
+// prefer not to use var because of issue in block cope and functional scope
+// do not declare variable without keywords 
+
+console.table([accId, accName, accPassword, accCity, accState]);
