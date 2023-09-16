85d9aa9e69ff15ee073daa19d2fe020b20720517
Sahil <leviack629@gmail.com>
2023-09-16 19:31:44 +0530

    datatypes conversion and operations

--- datatypeConversion.js
+++ datatypeConversion.js
@@ -34,7 +34,46 @@
 
 
 
-let someNumber = 12
-let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber)
-console.log(typeof(stringSomeNumber))
+// let someNumber = 12
+// let stringSomeNumber = String(someNumber)
+// console.log(stringSomeNumber)
+// console.log(typeof(stringSomeNumber))
+
+
+// ******operation*******
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(3%2)
+// console.log(2**3)
+
+// let str1 = "Emir"
+// let str2 = " Malik"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log((3+4) * (5 %3))
+// console.log(true)
+// console.log(+true)  //avoid this
+// console.log(+"")
+
+// let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter)
+
+
+
+// let x = 3;
+// const y = x++ //increments and returns the value before incrementing.
+// const y = ++x  //increments and returns the value after incrementing.
+// console.log(`x=${x} and y=${y}`)
