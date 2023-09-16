0813ca0516d1256a428a9d9e10cba667953126b6
Sahil <leviack629@gmail.com>
2023-09-16 18:16:19 +0530

    datatypes conversion

---
+++ datatypeConversion.js
@@ -0,0 +1,40 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+// let score = "Emir"
+
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) // NaN
+
+
+//"33" => 33
+//"33abc" => NaN
+//boolean => 1 or 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+// let isLoggedIn = "Emir"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(booleanIsLoggedIn))
+// console.log(booleanIsLoggedIn)
+
+
+//1 => true and 0 => false
+//"" => false
+//"Emir" => true
+
+
+
+
+let someNumber = 12
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber)
+console.log(typeof(stringSomeNumber))
