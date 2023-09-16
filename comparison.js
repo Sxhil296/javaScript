d98f8262cf59623a79af7ce0e2fb36f11a8e1f30
Sahil <leviack629@gmail.com>
2023-09-16 19:42:03 +0530

    comparison

---
+++ comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2 != 1)
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0) //null is converted into zero here
+
+
+//comparison and equality check works differently
+
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+
+//strict check doesn't convert the datatype
+// console.log("2" === 2)
+
+//avoid comparison between different data types
