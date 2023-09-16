26ce7eb1a6df795737aba5c3185e4e9394e01f2b
Sahil <leviack629@gmail.com>
2023-09-16 18:15:59 +0530

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer js or version
+
+
+// alert("hello")  //runs in browser not in nodejs
+
+let name = "Emir"
+let age = 20
+let isMale = true
+let state;
+
+//number
+//bigInt
+//string  - single quotes or double quotes
+//boolean - true or false
+//null - standalone value -it is a object
+//undefined - declared but not initialized - when value if not assigned - type is undefined
+//symbol -unique
+
+//object 
+
+console.log(typeof "Patrick")
+console.log(typeof undefined)
+console.log(typeof null)
