e783fa71988d970f6c53281c59376598370c6001
Sahil <leviack629@gmail.com>
2023-09-17 09:48:59 +0530

    array

---
+++ array.js
@@ -0,0 +1,41 @@
+//array
+
+const myArray = [0,1,2,3,4,5]
+// console.log(myArray[1])
+
+const myArray2 = new Array(2,4,6,8,10)
+// console.log(myArray2)
+
+
+//array methods
+// myArray.push(6)  //adds value in the end
+// console.log(myArray)
+// myArray.pop()  //deletes the end element
+// console.log(myArray)
+
+// myArray.unshift(9) //adds value in the beginning
+// console.log(myArray)
+// myArray.shift()  //deletes the first element
+// console.log(myArray)
+
+
+// console.log(myArray.includes(9))
+// console.log(myArray.indexOf(4))
+
+// const newArray = myArray.join()  //converts array into string
+// console.log(newArray)
+// console.log(myArray)
+// console.log(typeof newArray)
+// console.log(typeof myArray)
+
+
+// console.log("Original Array - ", myArray)
+// const slicedArray = myArray.slice(1,3)
+// console.log(slicedArray)
+// console.log("Original array after slice - ", myArray)
+
+// console.log("Original Array - ", myArray)
+// const splicedArray = myArray.splice(1,3)
+// console.log(splicedArray)
+// console.log("Original array after splice - ", myArray)
+
