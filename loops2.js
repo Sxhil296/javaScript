36aef66cd890d3f6eb510b7417d563e161bdae8d
Sahil <leviack629@gmail.com>
2023-09-20 11:55:55 +0530

    while do-while

---
+++ loops2.js
@@ -0,0 +1,30 @@
+//while
+
+
+// let i =0
+// while (i <= 10) {
+//     console.log(`value of i ${i}`)
+//     i = i+2
+// }
+
+
+
+
+// const myArray = ['one', 'two', 'three']
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(myArray[arr])
+//     arr++
+// }
+
+
+
+//do-while
+
+// let score = 1
+let score = 11
+do {
+    console.log(score)
+    score = score+1
+} while (score<=10);
