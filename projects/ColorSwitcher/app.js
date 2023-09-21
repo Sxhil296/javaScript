7a2a0a0064ced859d99224e39fa8813cfb0da0b2
Sahil <leviack629@gmail.com>
2023-09-21 10:37:08 +0530

    Color switcher

---
+++ projects/ColorSwitcher/app.js
@@ -0,0 +1,40 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+  // console.log(button)
+  button.addEventListener("click", (e) => {
+    // console.log(e)
+    // console.log(e.target)
+
+    // by if-else
+    // if(e.target.id === 'gray'){
+    //     body.style.backgroundColor =  e.target.id
+    // }
+    // if(e.target.id === 'white'){
+    //     body.style.backgroundColor =  e.target.id
+    // }
+    // if(e.target.id === 'blue'){
+    //     body.style.backgroundColor =  e.target.id
+    // }
+    // if(e.target.id === 'yellow'){
+    //     body.style.backgroundColor =  e.target.id
+    // }
+
+    //by switch case
+    switch (e.target.id) {
+      case "gray":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "white":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "blue":
+        body.style.backgroundColor = e.target.id;
+        break;
+      case "yellow":
+        body.style.backgroundColor = e.target.id;
+        break;
+    }
+  });
+});
