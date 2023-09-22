cdcbfa51084db2019d675108d2ec196c83d81f51
Sahil <leviack629@gmail.com>
2023-09-22 23:20:59 +0530

    promise, async await, fetch

--- advance/promises.js
+++ advance/promises.js
@@ -100,3 +100,27 @@ async function consumePromiseFive() {
 }
 
 consumePromiseFive();
+
+//********FETCH*****//
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log("ERROR : ", error);
+//   }
+// }
+
+// getAllUsers();
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
